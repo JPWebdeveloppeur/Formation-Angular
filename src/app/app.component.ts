f72a9a48ae856dcebd9941ec11ca81191d222ded
jonathan peraldo <jpwebdeveloppeur@gmail.com>
2022-05-10 15:56:23 +0200

    Gestion des routes & navigation au sein d'un projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import {POKEMONS} from "./mock-pokemon";
-import {Pokemon} from "./pokemon";
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl:'app.component.html'
 })
-export class AppComponent {
-  pokemonList:Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon | undefined;
-
-  ngOnInit(){
-    console.table(this.pokemonList);
-  }
-
-  SelectPokemon(pokemonId: string){
-    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
-
-    if (pokemon){
-      this.pokemonSelected = pokemon;
-    }else {
-      this.pokemonSelected = pokemon;
-    }
-  }
-
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BorderCardDirective } from './border-card.directive';
 import { PokemeonTypeColorPipe } from './pokemeon-type-color.pipe';
+import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
+import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BorderCardDirective,
-    PokemeonTypeColorPipe
+    PokemeonTypeColorPipe,
+    ListPokemonComponent,
+    DetailPokemonComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,

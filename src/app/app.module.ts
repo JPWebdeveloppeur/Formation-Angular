43b759c3b1509baf905776cf2b0019914534a8b4
jonathan peraldo <jpwebdeveloppeur@gmail.com>
2022-05-10 14:01:51 +0200

    création de pipe personalisé(ex:date ou type de pokémon)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BorderCardDirective } from './border-card.directive';
+import { PokemeonTypeColorPipe } from './pokemeon-type-color.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BorderCardDirective
+    BorderCardDirective,
+    PokemeonTypeColorPipe
   ],
   imports: [
     BrowserModule,

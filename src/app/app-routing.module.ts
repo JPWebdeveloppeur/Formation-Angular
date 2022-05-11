ea22b666c357a50ab201cf5ae53f26e7d8ec058a
jonathan peraldo <jpwebdeveloppeur@gmail.com>
2022-05-11 11:03:14 +0200

    Structurer l'architecture de l'application via la création d'un module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {ListPokemonComponent} from "./list-pokemon/list-pokemon.component";
-import {DetailPokemonComponent} from "./detail-pokemon/detail-pokemon.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
-// Pour la déclartaion de route mettre les routes spécifique en haut
-// et les global a la suite sinon on risque d'avoir des conflit
 const routes: Routes = [
-    { path: 'pokemons', component: ListPokemonComponent },
-    { path: 'pokemon/:id', component: DetailPokemonComponent },
     { path: '', redirectTo: 'pokemons', pathMatch: 'full' },
     { path: '**', component: PageNotFoundComponent}
 ];
---
+++ src/app/pokemon/pokemon.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {ListPokemonComponent} from "./list-pokemon/list-pokemon.component";
+import {DetailPokemonComponent} from "./detail-pokemon/detail-pokemon.component";
+import {BorderCardDirective} from "./border-card.directive";
+import {PokemeonTypeColorPipe} from "./pokemeon-type-color.pipe";
+import {RouterModule, Routes} from "@angular/router";
+import {PageNotFoundComponent} from "../page-not-found/page-not-found.component";
+
+const pokemonRoutes: Routes = [
+    { path: 'pokemons', component: ListPokemonComponent },
+    { path: 'pokemon/:id', component: DetailPokemonComponent },
+];
+
+@NgModule({
+  declarations: [
+      ListPokemonComponent,
+      DetailPokemonComponent,
+      BorderCardDirective,
+      PokemeonTypeColorPipe
+  ],
+  imports: [
+    CommonModule,
+      RouterModule.forChild(pokemonRoutes)
+  ]
+})
+export class PokemonModule { }

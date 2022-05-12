ed171f7c6ad5c3cf645430e99177618f024c95f3
jonathan peraldo <jpwebdeveloppeur@gmail.com>
2022-05-12 11:35:03 +0200

    (Formulaire)Creéation de formulaire d'edition d'un pokemon

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -5,10 +5,13 @@ import {DetailPokemonComponent} from "./detail-pokemon/detail-pokemon.component"
 import {BorderCardDirective} from "./border-card.directive";
 import {PokemeonTypeColorPipe} from "./pokemeon-type-color.pipe";
 import {RouterModule, Routes} from "@angular/router";
-import {PageNotFoundComponent} from "../page-not-found/page-not-found.component";
 import {PokemonService} from "./pokemon.service";
+import {FormsModule} from "@angular/forms";
+import { PokemonFormComponent } from './pokemon-form/pokemon-form.component';
+import { EditPokemonComponent } from './edit-pokemon/edit-pokemon.component';
 
 const pokemonRoutes: Routes = [
+    { path: 'edit/pokemon/:id', component: EditPokemonComponent },
     { path: 'pokemons', component: ListPokemonComponent },
     { path: 'pokemon/:id', component: DetailPokemonComponent },
 ];
@@ -18,11 +21,14 @@ const pokemonRoutes: Routes = [
       ListPokemonComponent,
       DetailPokemonComponent,
       BorderCardDirective,
-      PokemeonTypeColorPipe
+      PokemeonTypeColorPipe,
+      PokemonFormComponent,
+      EditPokemonComponent
   ],
   imports: [
     CommonModule,
-      RouterModule.forChild(pokemonRoutes)
+    FormsModule,
+    RouterModule.forChild(pokemonRoutes)
   ],
   providers: [PokemonService]
 })

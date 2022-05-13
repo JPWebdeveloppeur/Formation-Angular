811c63a6c6d6104e7c0703a50290f9dd68e41a5c
jonathan peraldo <jpwebdeveloppeur@gmail.com>
2022-05-13 16:38:08 +0200

    Authentification & Sécurité

---
+++ src/app/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import {Observable, of} from "rxjs";
+import {delay, tap} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+ isLoggedIn: boolean = false;
+ redirectUrl: string;
+
+ login(name: string, password:string): Observable<boolean>{
+    const isLoggedIn = (name == 'Jpdev' && password == 'Jpdev');
+
+    return of(isLoggedIn).pipe(delay(1000), tap(isLoggedIn => this.isLoggedIn = isLoggedIn));
+ }
+
+ logout(){
+     this.isLoggedIn = false;
+ }
+}
